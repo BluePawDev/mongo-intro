a9afc96f2ee40ff725f15113fb8eddbea2884db7
BluePawDev <jaruby@me.com>
2017-06-16 10:23:52 -0500

    In-Atom commit test

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 console.log('In server.js');
+
+// requires
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var index = require('./modules/index');
+
+
+
+// uses
+app.use = express.static('public');
+app.use = bodyParser.urlencoded({
+	extended: true
+});
+app.use = ;
+
+
+// globals
+var port = process.env.PORT || 3355;
+
+
+// spin up server
+app.listen(port, function() {
+	console.log('server up on:', port);
+});
